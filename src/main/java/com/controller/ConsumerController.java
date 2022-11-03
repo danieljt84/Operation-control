2d24085c72778f00a1ff000afc4309d9dc8d3dad
Daniel Moreira <73500174+danieljt84@users.noreply.github.com>
2022-11-03 10:25:04 -0300

    alt

--- src/main/java/com/controller/ConsumerController.java
+++ src/main/java/com/controller/ConsumerController.java
@@ -6,13 +6,20 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,6 +33,7 @@ import com.repository.DataTaskRepository;
 import com.repository.PromoterRepository;
 import com.service.DataTaskService;
 import com.service.ExcelService;
+import com.service.PromoterService;
 import com.service.api.ApiEmployeeService;
 import com.service.api.ApiLaivonService;
 import com.util.ProjectAdapter;
@@ -34,28 +42,40 @@ import com.util.ProjectAdapter;
 public class ConsumerController {
 
 	ConfigurableApplicationContext context;
-	
-	@Autowired
-	DataTaskRepository dataTaskRepository;
-	@Autowired
-	PromoterRepository promoterRepository;
 	@Autowired
 	DataTaskService dataTaskService;
+	@Autowired
+	PromoterService promoterService;
 	@Autowired
 	ApiLaivonService apiService;
 	@Autowired
 	ApiEmployeeService apiEmployeeService;
 	@Autowired
 	ExcelService excelService;
-
+	private static Logger logger = LoggerFactory.getLogger(RoutineController.class);
+	
 	public ConsumerController(ConfigurableApplicationContext context) {
 		this.context = context;
 	}
 
-	public void routine() {
+	@Scheduled(fixedDelay =3600000, initialDelay = 10000)
+	public void run() {
+		if(isBeforeMin() && isAfterMax()) {
+			routineToConsumer(0);
+			logger.info("BASE ATUALIZADA EM: " + LocalDateTime.now().toString());
+		}
+	}
+	@Scheduled(cron = "* 0 23 * * *")
+	public void run2() {
+		routineToConsumer(3);
+		logger.info("BASE DE 3 DIAS ANTERIORES ATUALIZADA EM: " + LocalDateTime.now().toString());
+	}
+	
+	private void routineToConsumer(long daysToSubtract) {
 		LocalDate endDate = LocalDate.now();
-		LocalDate startDate = endDate.minusDays(3);
+		LocalDate startDate = endDate.minusDays(daysToSubtract);
 		long daysBetween = ChronoUnit.DAYS.between(startDate, endDate);
+		Set<Long> idsPromoters = new HashSet<>();
 		for (int i = 0; i <= daysBetween; i++) {
 			for (ProjectAdapter project : ProjectAdapter.values()) {
 				List<DataTask> datas = apiService.getResume(project.getDescription(), startDate.plusDays(i));
@@ -64,12 +84,30 @@ public class ConsumerController {
 					dataTaskService.calculateInfoDataTask(data);
 					dataTaskService.defSituationDataTask(data);
 					dataTaskService.checkAndSaveDataTask(data);
-<<<<<<< Updated upstream
-=======
-					idsPromoters.add(data.getPromoter().getId());
->>>>>>> Stashed changes
+					idsPromoters.add(data.getId());
 				}
 			}
+		}
+		promoterService.updateStatusToInativo(idsPromoters);
+	}
+	
+	private static boolean isBeforeMin() {
+		LocalTime agora = LocalTime.now();
+		LocalTime limite = LocalTime.parse("22:00", DateTimeFormatter.ISO_TIME);
+		if (agora.isBefore(limite)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private static boolean isAfterMax() {
+		LocalTime agora = LocalTime.now();
+		LocalTime limite = LocalTime.parse("08:00", DateTimeFormatter.ISO_TIME);
+		if (agora.isAfter(limite)) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 }
--- src/main/java/com/service/PromoterService.java
+++ src/main/java/com/service/PromoterService.java
@@ -1,63 +1,34 @@
 package com.service;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.model.Promoter;
-import com.model.Team;
 import com.repository.PromoterRepository;
-import com.util.Status;
 
 @Service
 public class PromoterService {
 	@Autowired
 	PromoterRepository promoterRepository;
 	
-	
 	public List<Promoter> findByTeam(String nameTeam){
 		return promoterRepository.findByNameTeam(nameTeam);
-	}
-	
-	public Promoter save(Promoter promoter) {
-		return promoterRepository.save(promoter);
 	}
 	   
 	public List<Promoter> findAll(){
 	  return promoterRepository.findAll();
 	}
-<<<<<<< Updated upstream
-
-=======
-	@Transactional
+	
 	public void updateStatusToInativo(Set<Long> idPromoterts) {
 		try {
 			promoterRepository.updateStatusToInativo(idPromoterts);
+
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
-	
-	public Promoter checkPromoter( String namePromoter) {
-		Promoter promoter = promoterRepository.findByName(namePromoter);
-		if (promoter == null) {
-			Promoter newPromoter = new Promoter(namePromoter);
-			promoter = promoterRepository.save(newPromoter);
-		}
-		return promoter;
-	}
-	
-	public void updateIfHasUpdate(Promoter promoter,Team team,String cpf, String idSystem) {
-		try {
-		     promoter.setTeam(team);
-		     promoter.setStatus(Status.ATIVO);
-		     promoter.setIdSystem(Long.parseLong(idSystem));
-		     promoterRepository.save(promoter);
-		}catch (Exception e) {
-			System.out.println(e);
-		}
-	}
->>>>>>> Stashed changes
 }
